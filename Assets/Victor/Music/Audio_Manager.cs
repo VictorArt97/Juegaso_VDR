7bd0ebc61c3d4cf5330fae5b4e08acbeb2321f6e
VictorArt97 <Bakalovvictor@gmail.com>
2024-11-30 23:26:33 +0100

    AudioManager
    Música de fondo en el titulo y el menú de configuración

---
+++ Assets/Victor/Music/Audio_Manager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio_Manager : MonoBehaviour
+{
+    [SerializeField] AudioSource sfx;
+    
+    void ReproducirSonido(AudioClip SonidoBoton)
+    {
+        sfx.PlayOneShot(SonidoBoton);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Victor/Script-Victor/Menu.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using static UnityEditor.ShaderData;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] GameObject menuPausa;
+    private bool pausa;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pausa == false)
+            {
+                pausa = true;
+                menuPausa.SetActive(true);
+
+                Time.timeScale = 0f;
+
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+
+            }
+        }
+    }
+
+    public void Reanudar ()
+    {
+        menuPausa.SetActive(false);
+        pausa = false;
+
+        Time.timeScale = 1;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void Empezar()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void Configuracion()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Salir()
+    {
+        Application.Quit();
+    }
+}
