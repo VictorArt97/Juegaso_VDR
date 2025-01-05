15620960abd26b448daa21c6d5c2d2e5733b098d
Daniel Rodrigo <danielrodrigo2004@gmail.com>
2025-01-05 11:41:47 +0100

    nueva generacion de tablero

--- Assets/Dani/scripts/Nuevo/Tablero.cs
+++ Assets/Dani/scripts/Nuevo/Tablero.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Tablero : MonoBehaviour
 {
@@ -10,7 +12,7 @@ public class Tablero : MonoBehaviour
 
     private void Awake()
     {
-        GenerarTodasLasCasillas(1, Total_Casillas_X, Total_Casillas_Y);
+        GenerarTodasLasCasillas(1.8f, Total_Casillas_X, Total_Casillas_Y);
 
 
     }
@@ -18,18 +20,39 @@ public class Tablero : MonoBehaviour
     private void GenerarTodasLasCasillas(float tamanioCasilla, int contadorCasillasX, int contadorCasillasY  )
     {
         casillas = new GameObject[contadorCasillasX, contadorCasillasY];
-        //for (int X = 0; X < contadorCasillasX; X++)
-        //for (int y = 0; y < contadorCasillasY; y++)
-       // casillas[X, y] = GenerarUnaCasilla(tamanioCasilla, X, y);
-
-
+        for (int X = 0; X < contadorCasillasX; X++)
+        {
+            for (int y = 0; y < contadorCasillasY; y++)
+            {
+                casillas[X, y] = GenerarUnaCasilla(tamanioCasilla, X, y);
+            }
+            
+        }
     }
-    private void GenerarUnaCasilla(float tamanioCasilla, int x, int y)
-    {
-        GameObject objetoCasilla = new GameObject(string.Format("x:{0}, y:{1}", x, y));
-        objetoCasilla.transform.parent= transform;
-        //return objetoCasilla;
 
-    
+    private GameObject GenerarUnaCasilla(float tamanioCasilla, int x, int y)
+    {
+        GameObject objetoCasilla = new GameObject(string.Format("x{0}, y:{1}", x, y));
+        objetoCasilla.transform.parent = transform;
+
+        Mesh mesh = new Mesh();
+        objetoCasilla.AddComponent<MeshFilter>().mesh = mesh;
+        objetoCasilla.AddComponent<MeshRenderer>();
+
+        Vector3[] vertices = new Vector3[4];
+        vertices[0] = new Vector3(x* tamanioCasilla, 0, y*tamanioCasilla);
+        vertices[1] = new Vector3(x* tamanioCasilla, 0, (y+1)*tamanioCasilla);
+        vertices[2] = new Vector3((x+1)* tamanioCasilla, 0, y*tamanioCasilla);
+        vertices[3] = new Vector3((x+1)* tamanioCasilla, 0, (y+1)*tamanioCasilla);
+
+        int[] tris = new int[]{0, 1, 2, 1, 3, 2};
+
+        mesh.vertices = vertices; 
+        mesh.triangles = tris;
+
+        objetoCasilla.AddComponent<BoxCollider>();
+
+        return objetoCasilla;
+        
     }
 }
