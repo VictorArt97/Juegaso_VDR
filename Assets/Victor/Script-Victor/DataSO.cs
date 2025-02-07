d47c952f7e06b09d3f049c8a691d942fea36a26c
VictorArt97 <Bakalovvictor@gmail.com>
2025-02-07 18:23:44 +0100

    scriptable objects

---
+++ Assets/Victor/Script-Victor/DataSO.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NuevosDatosPersonaje", menuName = "DatosPersonaje")]
+public class DatosPersonaje : ScriptableObject  
+{
+    public float vida;
+    public float vidaMaxima;
+    
+    public float defensa;
+    public float defensaMaxima;
+    public bool vivo;
+    
+    public float danio;
+}
